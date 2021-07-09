11191c0408a4546d0dc6b75083b02c48565c7ce8
Finley Cooper <contact@finleycooper.co.uk>
2021-07-09 21:11:47 +0100

    Added favicon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const port = process.env.PORT || 8000;
 
 const app = express();
-app.use("/static", express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html", {root: __dirname});
