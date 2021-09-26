4c8b12f0d59bb45406b10b5622a842d61d265357
Finley Cooper <contact@finleycooper.co.uk>
2021-09-26 13:58:26 +0100

    Added ./ for clarity

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 const port = process.env.PORT || 8000;
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html", {root: __dirname});
