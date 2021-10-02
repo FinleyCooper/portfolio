665f110c380fefbc4d461f3535faced001ad8b96
Finley Cooper <contact@finleycooper.co.uk>
2021-10-02 15:22:39 +0100

    Updated Contact me page

--- index.js
+++ index.js
@@ -2,16 +2,43 @@
 
 
 const express = require("express");
-require("dotenv").config()
+const fetch = require("node-fetch").default;
+require("dotenv").config();
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html", {root: __dirname});
 });
+
+app.post("/api/contact", (req, res) => {
+    const name = getCleanString(req.body.name, 100, "No valid name given");
+    const email = getCleanString(req.body.email, 100, "No valid email given");
+    const message = getCleanString(req.body.message, 1000, "No valid message given");
+
+    fetch(`https://discord.com/api/webhooks/893393198687338497/${process.env.DISCORD_WEBHOOK_TOKEN}`, {
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: `Email: ${email}\nMessage: ${message}`,
+            username: name,
+        }),
+        method: "POST"
+    })
+    .then(resp => res.sendStatus(resp.status))
+    .catch(error => {
+        res.sendStatus(500);
+        console.error(error);
+    });
+
+    function getCleanString(text, maxLength, defualtText) {
+        return (typeof text === "string" && text.length < maxLength) ? text : defualtText;
+    }
+})
 
 app.listen(port, () => {
     console.log(process.env.NODE_ENV === "production" ? `App Running in Production mode on port ${port}` : `Developer app running at http://localhost:${port}`);
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,69 +3,90 @@
 
 const waitFor = ms => { return new Promise(resolve => setTimeout(resolve, ms)); }
 
-const data = [{ skill: "React", level: 1 }, { skill: "SQL", level: 2 }, { skill: "Git", level: 3 }, { skill: "Python", level: 4 }, { skill: "CSS", level: 5 }, { skill: "HTML", level: 5 }, { skill: "Javascript", level: 6 }];
-const margin = { top: 50, right: 25, bottom: 15, left: 100 };
+// skills graph
+(function () {
+    const data = [{ skill: "React", level: 1 }, { skill: "SQL", level: 2 }, { skill: "Git", level: 3 }, { skill: "Python", level: 4 }, { skill: "CSS", level: 5 }, { skill: "HTML", level: 5 }, { skill: "Javascript", level: 6 }];
+    const margin = { top: 50, right: 25, bottom: 15, left: 100 };
+    
+    const width = 1200 - margin.left - margin.right;
+    const height = 500  - margin.top - margin.bottom;
+    
+    const svg = d3.select(".chart-container").append("svg")
+    .attr("viewBox", `0 0 1800 600`)
+    .attr("class", "skills-chart")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    const x = d3.scale.linear()
+    .range([0, width])
+    .domain([0, 10]);
+    
+    const y = d3.scale.ordinal()
+    .rangeRoundBands([height, 0], 0.1)
+    .domain(data.map(d => {
+        return d.skill;
+    }));
+    
+    const yAxis = d3.svg.axis()
+    .scale(y)
+    .tickSize(0)
+    .orient("left");
+    
+    const gy = svg.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis)
+    
+    const bars = svg.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("g")
+    
+    bars.append("rect")
+    .attr("class", "bar")
+    .attr("y", d => {
+        return y(d.skill);
+    })
+    .attr("height", y.rangeBand())
+    .attr("x", 0)
+    .attr("width", d => {
+        return x(d.level);
+    });
+    
+    bars.append("text")
+    .attr("class", "label")
+    .attr("y", d => {
+        return y(d.skill) + y.rangeBand() / 2 + 4;
+    })
+    .attr("x", d => {
+        return x(d.level) + 3;
+    })
+    .text(d => {
+        return d.level;
+    });
+})();
 
-const width = 1200 - margin.left - margin.right;
-const height = 500  - margin.top - margin.bottom;
+// contact page
+(function () {
+    console.log("h")
+    document.querySelector(".submit").addEventListener("click", e => {
+        console.log("hj")
+        e.preventDefault();
+        fetch("/api/contact", {
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: document.querySelector(".name").value,
+                email: document.querySelector(".email").value,
+                message: document.querySelector(".message").value
+            }),
+            method: "POST"
+        });
+    });
+})();
 
-const svg = d3.select(".chart-container").append("svg")
-.attr("viewBox", `0 0 1800 600`)
-.attr("class", "skills-chart")
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-const x = d3.scale.linear()
-.range([0, width])
-.domain([0, 10]);
-
-const y = d3.scale.ordinal()
-.rangeRoundBands([height, 0], 0.1)
-.domain(data.map(d => {
-    return d.skill;
-}));
-
-const yAxis = d3.svg.axis()
-.scale(y)
-.tickSize(0)
-.orient("left");
-
-const gy = svg.append("g")
-.attr("class", "y-axis")
-.call(yAxis)
-
-const bars = svg.selectAll(".bar")
-.data(data)
-.enter()
-.append("g")
-
-bars.append("rect")
-.attr("class", "bar")
-.attr("y", d => {
-    return y(d.skill);
-})
-.attr("height", y.rangeBand())
-.attr("x", 0)
-.attr("width", d => {
-    return x(d.level);
-});
-
-bars.append("text")
-.attr("class", "label")
-.attr("y", d => {
-    return y(d.skill) + y.rangeBand() / 2 + 4;
-})
-.attr("x", d => {
-    return x(d.level) + 3;
-})
-.text(d => {
-    return d.level;
-});
-
-document.querySelectorAll("#age").forEach(elem => elem.innerText = Math.floor((new Date() - new Date(1149289200000)) / (1000 * 60 * 60 * 24 * 365.25)).toString());
-
-(async function() {
-
+// animation
+(async function () {
+    
     // typewrite effect
     await waitFor(2800);
     
@@ -73,28 +94,28 @@ document.querySelectorAll("#age").forEach(elem => elem.innerText = Math.floor((n
     textToComment.classList.remove("type-writer");
     textToComment.classList.add("comment");
     document.querySelector("#double-slash").style.display = "initial";
-
+    
     await waitFor(600);
-
+    
     // slide container right
     let container = document.querySelector(".title-container");
     container.classList.add("slide-left");
-
+    
     await waitFor(1500);
 
     // show fixed svg
     let { svg } = getFixedSVGParts();
-
+    
     svg.classList.remove("hidden");
     svg.classList.add("unhiding");
-
+    
     await waitFor(4000);
-
+    
     svg.classList.remove("unhiding")
     svg.classList.add("first-state")
-
+    
     fixedSVGHandlers();
-
+    
 })();
 
 function fixedSVGHandlers() {
@@ -110,10 +131,10 @@ function fixedSVGHandlers() {
         centerCircle.classList.add("enlarge-shrink");
         hexagon.classList.add("spin");
     }
-
+    
     svg.addEventListener("mouseover", startSpin);
     svg.addEventListener("mouseleave", () => { hexagon.addEventListener("animationiteration", stopSpin); });
-
+    
     svg.addEventListener("click", () => {
         if (!Array.from(svg.classList).includes("first-state")) return;
         centerCircle.addEventListener("animationiteration", stopSpin, {once: true})
@@ -134,3 +155,5 @@ function getFixedSVGParts() {
     let svg = document.querySelector("#spinning-circle")
     return { outerCircle, centerCircle, hexagon, svg }
 }
+
+document.querySelectorAll("#age").forEach(elem => elem.innerText = Math.floor((new Date() - new Date(1149289200000)) / (1000 * 60 * 60 * 24 * 365.25)).toString());
